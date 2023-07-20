051ec519ec5fe745236d6fe9bf290553bc6d6e02
Enndfp <3193947435@qq.com>
2023-07-20 09:26:35 +0800

    删除特定值元素

---
+++ src/main/java/com/enndfp/charpter1_linklist/level2/topic2_5删除元素/DeletePoint.java
@@ -0,0 +1,39 @@
+package com.enndfp.charpter1_linklist.level2.topic2_5删除元素;
+
+/**
+ * @author Enndfp
+ */
+public class DeletePoint {
+    /**
+     * 删除特定值元素
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode cur = dummyHead;
+
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummyHead.next;
+    }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
