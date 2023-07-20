ec3f71f347c5ff72213ca3664d76a33f687a9635
Enndfp <3193947435@qq.com>
2023-07-20 09:59:56 +0800

    删除链表的倒数第 N 个结点

---
+++ src/main/java/com/enndfp/charpter1_linklist/level2/topic2_5删除元素/RemoveNthFromEnd.java
@@ -0,0 +1,43 @@
+package com.enndfp.charpter1_linklist.level2.topic2_5删除元素;
+
+/**
+ * @author Enndfp
+ */
+public class RemoveNthFromEnd {
+
+    /**
+     * 删除链表的倒数第 N 个结点
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode slow = dummyHead;
+        ListNode fast = head;
+
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+
+        return dummyHead.next;
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
