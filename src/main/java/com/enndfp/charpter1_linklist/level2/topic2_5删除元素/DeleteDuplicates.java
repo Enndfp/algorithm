b1151f3f58fa90a1481687c027e2b7eef75826dd
Enndfp <3193947435@qq.com>
2023-07-21 09:37:53 +0800

    删除链表中重复元素（全都不要）

---
+++ src/main/java/com/enndfp/charpter1_linklist/level2/topic2_5删除元素/DeleteDuplicates.java
@@ -0,0 +1,46 @@
+package com.enndfp.charpter1_linklist.level2.topic2_5删除元素;
+
+/**
+ * 删除链表中的重复元素,全都不要
+ *
+ * @author Enndfp
+ */
+public class DeleteDuplicates {
+    /**
+     * 删除链表中的重复元素,全都不要
+     *
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode cur = dummyHead;
+
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummyHead.next;
+    }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
