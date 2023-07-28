a4f58f223545383f14faa95b9c171a2d0e72d7c4
Enndfp <3193947435@qq.com>
2023-07-28 21:53:29 +0800

    二分查找在数组找出插入位置

---
+++ src/main/java/com/enndfp/charpter3_array/level1/SearchInsert.java
@@ -0,0 +1,30 @@
+package com.enndfp.charpter3_array.level1;
+
+/**
+ * 在数组中寻找插入位置
+ *
+ * @author Enndfp
+ */
+public class SearchInsert {
+    /**
+     * 二分查找
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
