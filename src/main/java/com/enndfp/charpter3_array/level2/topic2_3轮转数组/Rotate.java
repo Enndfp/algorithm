f2809ca22f5bd73500d96e8621a723ffc3c77b22
Enndfp <3193947435@qq.com>
2023-08-01 20:01:09 +0800

    轮转数组

---
+++ src/main/java/com/enndfp/charpter3_array/level2/topic2_3轮转数组/Rotate.java
@@ -0,0 +1,25 @@
+package com.enndfp.charpter3_array.level2.topic2_3轮转数组;
+
+/**
+ * 轮转数组
+ *
+ * @author Enndfp
+ */
+public class Rotate {
+    public void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+            start++;
+            end--;
+        }
+    }
+}
